fe1609bc6efc24fa467a010b5b7138a8a9ff741c
Chiro Hiro <chiro@fkguru.com>
2018-04-25 11:33:41 +0700

    Update hash libname

--- utilities/hash.js
+++ utilities/hash.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const crypto = require('crypto');
-const adler32 = require('adler32');
-const ripemd160 = require('ripemd160');
+const adler32lib = require('adler32');
+const ripemd160lib = require('ripemd160');
 
 function sha256(value, encode) {
     return crypto.createHash('sha256').update(value).digest(encode || undefined);
@@ -10,13 +10,13 @@ function sha256(value, encode) {
 
 function adler32(value, encode) {
     if (typeof (encode) !== 'undefined' && encode === 'hex') {
-        return adler32.sum(value).toString(16);
+        return adler32lib.sum(value).toString(16);
     }
-    return Buffer.from(adler32.sum(value).toString(16), 'hex');
+    return Buffer.from(adler32lib.sum(value).toString(16), 'hex');
 }
 
 function ripemd160(value, encode) {
-    return new ripemd160().update(value).digest(encode || undefined);
+    return new ripemd160lib().update(value).digest(encode || undefined);
 }
 
 module.exports = {sha256, adler32, ripemd160};
